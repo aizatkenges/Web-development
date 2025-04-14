c76b7d7d74e38de608afa83da80934b4e510fc17
Aizat <aizatkenges@gmail.com>
2025-04-14 21:38:21 +0500

    Front-end finished

--- Blog_Dive_Uni/BlogDive/src/app/components/dashboard/dashboard.component.ts
+++ Blog_Dive_Uni/BlogDive/src/app/components/dashboard/dashboard.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { Router } from '@angular/router';
-=======
 
->>>>>>> 39df03afda20cb9ed8290d7a0e0378500b0c4153
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-<<<<<<< HEAD
+
 
 export class DashboardComponent {
   constructor(private router: Router) {}
@@ -20,8 +19,3 @@ export class DashboardComponent {
     this.router.navigate(['/login']);
   }
 }
-=======
-export class DashboardComponent {
-
-}
->>>>>>> 39df03afda20cb9ed8290d7a0e0378500b0c4153
